57b289e3c7e1361c2412c54fa99d14938fbf3c86
dushegubka <andrey@andreev.ru>
2024-03-29 01:12:58 +0300

    Добавил доменную модель Application.cs и тесты к ней ApplicationDomainTests.cs

---
+++ src/Domain/Applications/Application.cs
@@ -0,0 +1,66 @@
+﻿using Domain.Enums;
+
+namespace Domain.Applications;
+
+/// <summary>
+/// Заявка на активность
+/// </summary>
+public class Application
+{
+
+    private Application(Guid id,
+        Guid authorId,
+        ActivityType activityType,
+        ApplicationTitle title,
+        ApplicationDescription description,
+        ApplicationOutline outline)
+    {
+        Id = id;
+        AuthorId = authorId;
+        ActivityType = activityType;
+        Title = title;
+        Description = description;
+        Outline = outline;
+    }
+    
+    /// <summary>
+    /// Id заявки
+    /// </summary>
+    public Guid Id { get; }
+    
+    /// <summary>
+    /// Id автора
+    /// </summary>
+    public Guid AuthorId { get; }
+    
+    /// <summary>
+    /// Тип активности
+    /// </summary>
+    public ActivityType ActivityType { get; private set; }
+    
+    /// <summary>
+    /// Заголовок заявки
+    /// </summary>
+    public ApplicationTitle Title { get; private set; }
+
+    /// <summary>
+    /// Краткое описание для сайта
+    /// </summary>
+    public ApplicationDescription Description { get; private set; }
+    
+    /// <summary>
+    /// План выступления
+    /// </summary>
+    public ApplicationOutline Outline { get; private set; }
+    
+    public static Application Create(
+        Guid id,
+        Guid authorId,  
+        ActivityType activityType,
+        ApplicationTitle title,
+        ApplicationDescription description,
+        ApplicationOutline outline)
+    {
+        return new Application(id, authorId, activityType, title, description, outline);
+    }
+}
---
+++ tests/Ocs.Domain.Tests/Applications/ApplicationDomainTests.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Applications;
+using Domain.Enums;
+using FluentAssertions;
+
+namespace Ocs.Domain.Tests.Applications;
+
+public class ApplicationDomainTests
+{
+    [Fact]
+    public void Application_Create_Should_Correctly_Fill_Required_Properties()
+    {
+        // Arrange
+        var applicationId = Guid.NewGuid();
+        var authorId = Guid.NewGuid();
+        var title = ApplicationTitle.Create("Новые фичи C# vNext");
+        var description = ApplicationDescription.Create("Расскажу что нас ждет в новом релизе!");
+        var outline = ApplicationOutline.Create("очень много текста... прямо детальный план доклада!");
+
+        // Act
+        var application = Application.Create(
+            applicationId,
+            authorId,
+            ActivityType.Report,
+            title,
+            description,
+            outline
+        );
+        
+        // Assert
+        application.Id.Should().Be(applicationId);
+        application.AuthorId.Should().Be(authorId);
+        application.ActivityType.Should().Be(ActivityType.Report);
+        application.Title.Should().Be(title);
+        application.Description.Should().Be(description);
+        application.Outline.Should().Be(outline);
+    }
+}
