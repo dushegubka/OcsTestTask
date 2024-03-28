417989b588c76fe094391feeedbdff70c8688433
dushegubka <andrey@andreev.ru>
2024-03-29 01:11:16 +0300

    Добавил утилитарный метод для валидации строковых свойств

---
+++ src/Domain/Utils/StringValidatorUtils.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Utils;
+
+public static class StringValidatorUtils
+{
+    public static void ValidateStringValue(string? value, int maxLength = int.MaxValue)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(value);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(value.Length, maxLength);
+    }
+}
