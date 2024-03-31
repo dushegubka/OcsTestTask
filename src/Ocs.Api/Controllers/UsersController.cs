0e7db4c21756275e146537faa3826e9806ec7bd8
dushegubka <andrey@andreev.ru>
2024-03-31 21:13:32 +0300

    Добавил UsersController.cs

---
+++ src/Ocs.Api/Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ocs.ApplicationLayer.Applications;
+using Ocs.ApplicationLayer.Exceptions;
+using Ocs.ApplicationLayer.Users;
+using Ocs.Domain.Applications;
+
+namespace Ocs.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _userService;
+    private readonly IApplicationRepository _applicationRepository;
+
+    public UsersController(IUserService userService, IApplicationRepository applicationRepository)
+    {
+        _userService = userService;
+        _applicationRepository = applicationRepository;
+    }
+
+    [HttpGet]
+    [Route("{userId:guid}/currentapplication")]
+    public async Task<ActionResult<ApplicationView>> GetDraftApplication(
+        Guid userId, CancellationToken cancellationToken)
+    {
+        var isUserExist = _applicationRepository.UserHasDraftApplication(userId);
+        
+        if (!isUserExist)
+        {
+            throw new UserNotFoundException(userId, "Пользователь не найден");
+        }
+        
+        var application = await _applicationRepository
+            .GetUserDraftApplicationAsync(userId, cancellationToken);
+        
+        if (application is null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(ApplicationView.Create(application));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<UserView>> CreateUser([FromBody] UserCreateView newUser, 
+        CancellationToken cancellationToken)
+    {
+        var result = await _userService.CreateAsync(newUser, cancellationToken);
+        
+        return Ok(result);
+    }
+}
