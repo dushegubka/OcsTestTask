a2b2c491efa9ac5e05ef7fd3b0b008d687eb29ce
dushegubka <andrey@andreev.ru>
2024-03-31 05:06:35 +0300

    Добавил забытый ErrorResponse.cs

---
+++ src/Ocs.Api/ErrorResponse.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ocs.Api;
+
+public class ErrorResponse
+{
+    public string Message { get; init; }
+
+    public int StatusCode { get; init; }
+
+    public ProblemDetails Details { get; init; }
+}
