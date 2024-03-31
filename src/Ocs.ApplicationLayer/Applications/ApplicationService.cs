7b55bc6352b163584e0e00b9ac060e69a4561fa7
dushegubka <andrey@andreev.ru>
2024-03-31 21:12:24 +0300

    Добавил проверку на наличие заявки в бд и метод, проверяющий есть ли у пользователя неотправленные заявки

--- src/Ocs.ApplicationLayer/Applications/ApplicationService.cs
+++ src/Ocs.ApplicationLayer/Applications/ApplicationService.cs
@@ -1,5 +1,6 @@
 ﻿using Ocs.ApplicationLayer.Exceptions;
 using Ocs.Domain.Applications;
+using Ocs.Domain.Users;
 using Ocs.Infrastructure.Applications;
 using Ocs.Infrastructure.Extensions;
 
@@ -8,10 +9,12 @@ namespace Ocs.ApplicationLayer.Applications;
 public class ApplicationService : IApplicationService
 {
     private readonly IApplicationRepository _repository;
+    private readonly IUserRepository _userRepository;
 
-    public ApplicationService(IApplicationRepository repository)
+    public ApplicationService(IApplicationRepository applicationRepository, IUserRepository userRepository)
     {
-        _repository = repository;
+        _repository = applicationRepository;
+        _userRepository = userRepository;
     }
     
     public async Task<ApplicationView?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
@@ -23,6 +26,18 @@ public class ApplicationService : IApplicationService
 
     public async Task<ApplicationView?> CreateAsync(ApplicationCreateView newApplication, CancellationToken cancellationToken = default)
     {
+        var user = await _userRepository.GetByIdAsync(newApplication.Author, cancellationToken);
+
+        if (user is null)
+        {
+            throw new UserNotFoundException(newApplication.Author, "Пользователь не найден");
+        }
+        
+        if (_repository.UserHasDraftApplication(newApplication.Author))
+        {
+            throw new UserAlreadyHasDraftApplicationException(newApplication.Author, "У пользователя уже есть черновик заявки");
+        }
+        
         var application = Application.Create(
             Guid.NewGuid(), 
             newApplication.Author,
@@ -83,6 +98,13 @@ public class ApplicationService : IApplicationService
 
     public async Task<bool> SubmitAsync(Guid id, CancellationToken cancellationToken = default)
     {
+        var application = await _repository.GetByIdAsync(id, cancellationToken);
+
+        if (application is null)
+        {
+            return false;
+        }
+        
         return await _repository.SubmitAsync(id, cancellationToken);
     }
 
@@ -98,5 +120,17 @@ public class ApplicationService : IApplicationService
         var applications = await _repository.GetUnsubmittedOlderAsync(date, cancellationToken);
         
         return applications.Select(ApplicationView.Create);
+    }
+
+    public async Task<bool> UserHasDraftApplicationAsync(Guid userId, CancellationToken cancellationToken = default)
+    {
+        var user = await _userRepository.GetByIdAsync(userId, cancellationToken);
+
+        if (user is null)
+        {
+            throw new UserNotFoundException(userId, "Пользователь не найден");
+        }
+        
+        return _repository.UserHasDraftApplication(userId);
     }
 }
