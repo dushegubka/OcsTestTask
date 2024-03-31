cdd39b1adb0af793450c9df43f173ea48bcc4535
dushegubka <andrey@andreev.ru>
2024-03-31 05:06:19 +0300

    Добавил ApplicationController

---
+++ src/Ocs.Api/Controllers/ApplicationsController.cs
@@ -0,0 +1,118 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+using Ocs.ApplicationLayer.Applications;
+using Ocs.ApplicationLayer.Exceptions;
+using Ocs.ApplicationLayer.Extensions;
+using Ocs.ApplicationLayer.Utils;
+
+namespace Ocs.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ApplicationsController : ControllerBase
+{
+    private readonly IApplicationService _applicationService;
+
+    public ApplicationsController(IApplicationService applicationService)
+    {
+        _applicationService = applicationService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ApplicationView>> CreateAsync([FromBody] ApplicationCreateView newApplication, CancellationToken cancellationToken)
+    {
+        var application = await _applicationService.CreateAsync(newApplication, cancellationToken);
+        
+        return Ok(application);
+    }
+    
+    [HttpPost]
+    [Route("{id:guid}/submit")]
+    public async Task<IActionResult> SubmitAsync([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var isSubmitted = await _applicationService.SubmitAsync(id, cancellationToken);
+        
+        if (!isSubmitted)
+        {
+            return NotFound();
+        }
+        
+        return Ok();
+    }
+
+    [HttpPut]
+    [Route("{id:guid}")]
+    public async Task<ActionResult<ApplicationView>> UpdateAsync([FromRoute] Guid id,
+        [FromBody] ApplicationEditView updatedApplication,
+        CancellationToken cancellationToken)
+    {
+        var updateResult = await _applicationService.UpdateAsync(id, updatedApplication, cancellationToken);
+
+        if (updateResult is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(updateResult);
+    }
+    
+    
+    [HttpGet]
+    [Route("{id:guid}")]
+    public async Task<ActionResult<ApplicationView>> GetByIdAsync([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var application = await _applicationService.GetByIdAsync(id, cancellationToken);
+        
+        if (application is null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(application);
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ApplicationView>>> GetSubmittedAfter(
+        [FromQuery] string submittedAfter, 
+        CancellationToken cancellationToken)
+    {
+        if (!DateTimeOffsetUtils.ParseFromQueryParam(submittedAfter, out var date))
+        {
+            return BadRequest();
+        }
+        
+        var result = await _applicationService.GetSubmittedAfterAsync(date, cancellationToken);
+        
+        return Ok(result);
+    }
+    
+    
+    [HttpGet("{unsubmittedOlder}")]
+    public async Task<ActionResult<IEnumerable<ApplicationView>>> GetUnsubmittedOlder(
+        string unsubmittedOlder, 
+        CancellationToken cancellationToken)
+    {
+        if (!DateTimeOffsetUtils.ParseFromQueryParam(unsubmittedOlder, out var date))
+        {
+            return BadRequest();
+        }
+        
+        var result = await _applicationService.GetUnsubmittedOlderAsync(date, cancellationToken);
+        
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    [Route("{id:guid}")]
+    public async Task<ActionResult<bool>> DeleteAsync([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var isDeleted = await _applicationService.DeleteAsync(id, cancellationToken);
+        
+        if (!isDeleted)
+        {
+            return NotFound();
+        }
+        
+        return Ok();
+    }
+}
---
+++ src/Ocs.ApplicationLayer/Utils/DateTimeOffsetUtils.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Ocs.ApplicationLayer.Utils;
+
+public static class DateTimeOffsetUtils
+{
+    public static bool ParseFromQueryParam(string value, out DateTimeOffset result)
+    {
+        const string format = "yyyy-MM-dd HH:mm:ss.ff";
+
+        value = value.Trim('"');
+
+        var parseResult = DateTimeOffset.TryParseExact(value, format,
+            CultureInfo.InvariantCulture, DateTimeStyles.None, out var date);
+
+        result = date;
+
+        return parseResult;
+    }
+}
