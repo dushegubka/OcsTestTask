3acce708d267a4110527c622cca4d2b0afd05c40
dushegubka <andrey@andreev.ru>
2024-04-01 03:45:22 +0300

    Добавил вызов EnsureCreated

--- src/Ocs.Infrastructure/ApplicationDbContext.cs
+++ src/Ocs.Infrastructure/ApplicationDbContext.cs
@@ -5,17 +5,16 @@ using Ocs.Infrastructure.Users;
 
 namespace Ocs.Infrastructure;
 
-public class ApplicationDbContext : DbContext
+public sealed class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        
+        Database.EnsureCreated();
     }
     
     public DbSet<ApplicationEntity> Applications { get; init; }
     
     public DbSet<UserEntity> Users { get; init; }
-    
 
     protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
     {
