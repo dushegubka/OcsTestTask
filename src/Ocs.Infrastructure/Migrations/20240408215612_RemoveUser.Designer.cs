02276f131b1efd1c693494a3b275bd11c1d2d9dc
dushegubka <andrey@andreev.ru>
2024-04-11 00:41:37 +0300

    Обновил миграцию бд с удалением пользователя из базы

--- src/Ocs.Infrastructure/Migrations/20240408215612_RemoveUser.Designer.cs
+++ src/Ocs.Infrastructure/Migrations/20240408215612_RemoveUser.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Ocs.Infrastructure;
@@ -11,9 +12,11 @@ using Ocs.Infrastructure;
 namespace Ocs.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240408215612_RemoveUser")]
+    partial class RemoveUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
